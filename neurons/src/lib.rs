9e199cf7da630cf9de8fde6ade4e58b140d8e3f3
Bartek Chaliński <b.chalinski@gmail.com>
2025-05-13 18:04:00 +0200

    neurons refactored to work as wasm

--- neurons/src/lib.rs
+++ neurons/src/lib.rs
@@ -4,153 +4,89 @@ pub mod prior_voting_history;
 pub mod quorum;
 pub mod trust_graph;
 pub mod trust_history;
-use assigned_reputation::AssignedReputationNeuron;
-use camino::Utf8Path;
+pub mod types;
+use assigned_reputation::{AssignedReputationNeuron, ReputationTier};
 use neurons::Neuron;
 use prior_voting_history::PriorVotingHistoryNeuron;
 use quorum::{normalize_votes, DelegateesForUser};
-use serde::{Deserialize, Serialize};
-use std::collections::{BTreeMap, HashMap};
-use std::fs::{self, File};
-use std::io::BufReader;
+use std::collections::HashMap;
 use trust_graph::TrustGraphNeuron;
 use trust_history::TrustHistoryNeuron;
+use types::{Submission, Vote, DECIMALS};
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn summarize(json_input: &str) -> String {
-    let parsed: Result<HashMap<String, Vec<i32>>, _> = serde_json::from_str(json_input);
-    match parsed {
-        Ok(map) => map
-            .into_iter()
-            .map(|(k, v)| format!("hello world {}: {}\n", k, v.iter().sum::<i32>()))
-            .collect(),
-        Err(_) => "Invalid input JSON.".to_string(),
-    }
-}
-
-pub const DECIMALS: i64 = 1_000_000_000_000_000_000;
-
-#[derive(Deserialize, Serialize, Debug, Clone, PartialEq, Eq, Hash, Ord, PartialOrd)]
-pub enum SubmissionCategory {
-    Applications,
-    FinancialProtocols,
-    InfrastructureAndServices,
-    DeveloperTooling,
-}
-
-#[non_exhaustive]
-#[derive(Deserialize, Serialize, Debug, Clone, PartialEq, Eq, Hash, Ord, PartialOrd)]
-pub struct Submission {
-    pub name: String,
-    pub category: SubmissionCategory,
-}
-
-impl Submission {
-    #[must_use]
-    pub fn new(name: String, category: SubmissionCategory) -> Self {
-        Self { name, category }
-    }
-}
-
-#[derive(Deserialize, Serialize, Debug, Clone, PartialEq)]
-pub enum Vote {
-    Yes,
-    No,
-    Delegate,
-    Abstain,
-}
-
-fn write_result<T>(file_name: &str, data: &T)
-where
-    T: Serialize,
-{
-    let serialized = serde_json::to_string(&data).unwrap();
-    fs::write(file_name, serialized).unwrap();
-}
-
-fn to_sorted_map<K, L>(data: HashMap<K, L>) -> BTreeMap<K, L>
-where
-    K: Ord,
-{
-    data.into_iter().collect()
-}
-
-#[allow(clippy::cast_precision_loss, clippy::cast_possible_truncation)]
-fn to_fixed_point_decimal(val: f64) -> i128 {
-    (val * DECIMALS as f64) as i128
-}
-
-fn calculate_neuron_results(users: &[String], neurons: Vec<Box<dyn Neuron>>) {
-    for neuron in neurons {
-        println!("running {}", neuron.name());
-        let result = neuron.calculate_result(users);
-        let result: HashMap<String, String> = result
-            .into_iter()
-            .map(|(key, value)| (key, to_fixed_point_decimal(value).to_string()))
-            .collect();
-        let result = to_sorted_map(result);
-        write_result(&format!("result/{}.json", neuron.name()), &result);
-    }
-}
-fn calculate_trust_neuron_results(users: &[String], neurons: Vec<Box<dyn Neuron>>) {
-    for neuron in neurons {
-        println!("running {}", neuron.name());
-        let result = neuron.calculate_result(users);
-        let result = to_sorted_map(result);
-        write_result(&format!("result/{}.json", neuron.name()), &result);
-    }
-}
-fn load_trust_data(path: &Utf8Path) -> anyhow::Result<HashMap<u32, HashMap<String, Vec<String>>>> {
-    let file = File::open(path)?;
-    let reader = BufReader::new(file);
-
-    let trust_data: HashMap<u32, HashMap<String, Vec<String>>> = serde_json::from_reader(reader)?;
-
-    Ok(trust_data)
-}
-fn remove_temp_files(current_round: u32) {
-    for i in current_round - 1..=current_round {
-        let path = format!("result/trust_graph_neuron_{i}.json");
-        println!("remove {path}");
-        fs::remove_file(&path).unwrap();
-    }
-}
-fn main() {
-    // prepare users data
-    let current_round = 35;
+pub fn run_neurons(
+    current_round: u32,
+    users_base: &str,
+    previous_rounds_for_users: &str,
+    users_reputation: &str,
+    trusted_for_user_per_round: &str,
+) -> Result<String, String> {
+    // parse all data
     // depending on which file is passed here, different users-base will be run through neurons
-    let users_raw = fs::read_to_string("data/all_users.json").unwrap();
-    let users: Vec<String> = serde_json::from_str(users_raw.as_str()).unwrap();
+    let users_base: Vec<String> = match serde_json::from_str(users_base) {
+        Ok(users_base) => users_base,
+        Err(err) => return Err(format!("users_base json parsing error {}", err.to_string())),
+    };
+    let previous_rounds_for_users: HashMap<String, Vec<u32>> =
+        match serde_json::from_str(previous_rounds_for_users) {
+            Ok(previous_rounds_for_users) => previous_rounds_for_users,
+            Err(err) => {
+                return Err(format!(
+                    "previous_rounds_for_users json parsing error {}",
+                    err.to_string()
+                ))
+            }
+        };
+    let users_reputation: HashMap<String, ReputationTier> =
+        match serde_json::from_str(users_reputation) {
+            Ok(users_reputation) => users_reputation,
+            Err(err) => {
+                return Err(format!(
+                    "users_reputation json parsing error {}",
+                    err.to_string()
+                ))
+            }
+        };
+    let trusted_for_user_per_round: HashMap<u32, HashMap<String, Vec<String>>> =
+        match serde_json::from_str(trusted_for_user_per_round) {
+            Ok(trusted_for_user_per_round) => trusted_for_user_per_round,
+            Err(err) => {
+                return Err(format!(
+                    "trusted_for_user_per_round json parsing error {}",
+                    err.to_string()
+                ))
+            }
+        };
+    // create neurons
+    let prior_voting_history_neuron =
+        PriorVotingHistoryNeuron::from_data(previous_rounds_for_users);
 
-    // prepare prior voting history neuron
-    let path = Utf8Path::new("data/previous_rounds_for_users.json");
-    let prior_voting_history_neuron = PriorVotingHistoryNeuron::try_from_file(path).unwrap();
-
-    // prepare reputation neuron
-    let path = Utf8Path::new("data/users_reputation.json");
-    let assigned_reputation_neuron = AssignedReputationNeuron::try_from_file(path).unwrap();
+    let assigned_reputation_neuron = AssignedReputationNeuron::from_data(users_reputation);
 
     // prepare and run trust neurons for previous rounds
-    let path = Utf8Path::new("data/trusted_for_user_per_round.json");
-    let trust_data = load_trust_data(path).unwrap();
     let mut trust_graph_neurons: Vec<Box<dyn Neuron>> = vec![];
-    trust_data.iter().for_each(|(round, trusted_for_user)| {
-        if *round == current_round || *round == current_round - 1 {
-            trust_graph_neurons.push(Box::new(TrustGraphNeuron::from_data(
-                trusted_for_user.clone(),
-                *round,
-            )));
-        }
-    });
-    calculate_trust_neuron_results(&users, trust_graph_neurons);
+    trusted_for_user_per_round
+        .iter()
+        .for_each(|(round, trusted_for_user)| {
+            if *round == current_round || *round == current_round - 1 {
+                trust_graph_neurons.push(Box::new(TrustGraphNeuron::from_data(
+                    trusted_for_user.clone(),
+                    *round,
+                )));
+            }
+        });
 
-    // prepare trust history neuron
-    let trust_history_neuron: TrustHistoryNeuron = TrustHistoryNeuron::new(current_round as usize);
+    let trust_graph_neurons_results: HashMap<String, HashMap<String, f64>> =
+        calculate_trust_neuron_results(&users_base, trust_graph_neurons);
+
+    let trust_history_neuron =
+        TrustHistoryNeuron::from_data(current_round as usize, trust_graph_neurons_results);
 
     // run all neurons
-    calculate_neuron_results(
-        &users,
+    let results = calculate_neuron_results(
+        &users_base,
         vec![
             Box::new(prior_voting_history_neuron),
             Box::new(assigned_reputation_neuron),
@@ -158,25 +94,79 @@ fn main() {
         ],
     );
 
-    do_normalize_votes();
-
-    remove_temp_files(current_round);
+    Ok(serde_json::to_string_pretty(&results).unwrap())
 }
 
-fn do_normalize_votes() {
-    let votes_raw = fs::read_to_string("data/votes.json").unwrap();
-    let votes: HashMap<String, HashMap<String, Vote>> =
-        serde_json::from_str(votes_raw.as_str()).unwrap();
+#[wasm_bindgen]
+pub fn run_votes_normalization(
+    votes: &str,
+    submissions: &str,
+    delegatees_for_user: &str,
+) -> Result<String, String> {
+    let votes: HashMap<String, HashMap<String, Vote>> = match serde_json::from_str(votes) {
+        Ok(votes) => votes,
+        Err(err) => return Err(format!("votes json parsing error {}", err.to_string())),
+    };
 
-    let submissions_raw = fs::read_to_string("data/submissions.json").unwrap();
-    let submissions: Vec<Submission> = serde_json::from_str(submissions_raw.as_str()).unwrap();
+    let submissions: Vec<Submission> = match serde_json::from_str(submissions) {
+        Ok(submissions) => submissions,
+        Err(err) => {
+            return Err(format!(
+                "submissions json parsing error {}",
+                err.to_string()
+            ))
+        }
+    };
 
-    let delegatees_for_user_raw = fs::read_to_string("data/delegatees_for_user.json").unwrap();
     let delegatees_for_user: HashMap<String, DelegateesForUser> =
-        serde_json::from_str(delegatees_for_user_raw.as_str()).unwrap();
-    let normalized_votes = normalize_votes(votes, &submissions, &delegatees_for_user).unwrap();
-    write_result(
-        "result/normalized_votes.json",
-        &to_sorted_map(normalized_votes),
-    );
+        match serde_json::from_str(delegatees_for_user) {
+            Ok(delegatees_for_user) => delegatees_for_user,
+            Err(err) => {
+                return Err(format!(
+                    "delegatees_for_user json parsing error {}",
+                    err.to_string()
+                ))
+            }
+        };
+
+    let normalized_votes = match normalize_votes(votes, &submissions, &delegatees_for_user) {
+        Ok(normalized_votes) => normalized_votes,
+        Err(err) => return Err(format!("error normalizing votes {}", err.to_string())),
+    };
+    Ok(serde_json::to_string_pretty(&normalized_votes).unwrap())
+}
+
+fn calculate_trust_neuron_results(
+    users: &[String],
+    neurons: Vec<Box<dyn Neuron>>,
+) -> HashMap<String, HashMap<String, f64>> {
+    let mut results: HashMap<String, HashMap<String, f64>> = HashMap::new();
+    for neuron in neurons {
+        // TODO maybe add fixed point decimal like in calculate_neuron_results()
+        let result = neuron.calculate_result(users);
+        results.insert(neuron.name(), result);
+    }
+    results
+}
+
+fn calculate_neuron_results(
+    users: &[String],
+    neurons: Vec<Box<dyn Neuron>>,
+) -> HashMap<String, HashMap<String, String>> {
+    let mut results: HashMap<String, HashMap<String, String>> = HashMap::new();
+    for neuron in neurons {
+        println!("running {}", neuron.name());
+        let result = neuron.calculate_result(users);
+        let result: HashMap<String, String> = result
+            .into_iter()
+            .map(|(key, value)| (key, to_fixed_point_decimal(value).to_string()))
+            .collect();
+        results.insert(neuron.name(), result);
+    }
+    results
+}
+
+#[allow(clippy::cast_precision_loss, clippy::cast_possible_truncation)]
+fn to_fixed_point_decimal(val: f64) -> i128 {
+    (val * DECIMALS as f64) as i128
 }
